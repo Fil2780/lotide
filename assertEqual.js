ddc860ce0521a0ccbeabf6f93db408756a6f6aee
Fil2780 <filgirma.hail@gmail.com>
2021-11-23 11:10:34 +0000

    assertEqual.js defined for comparing

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log("💚Assertion Passed: " + actual + " === " + expected);
+  }
+  else {
+    console.log("💔Assertion Failed: " + actual + " !== " + expected);
+  }
+  return assertEqual;
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Pho", "Pho");
+assertEqual(1,1);
+assertEqual(899,889);
