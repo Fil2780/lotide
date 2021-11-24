dacf0b161f660e49791e8455dc194126b8d098d5
Fil2780 <filgirma.hail@gmail.com>
2021-11-24 23:56:58 +0000

    edited tail.js

--- tail.js
+++ tail.js
@@ -1,20 +1,18 @@
-const assertEqual = function(actual, expected) {
-  const tail = function(actual) {
-    let act = actual.slice(1);
-    return (act);
-  };
-  console.log(tail(actual));
-  if (actual === expected) {
-    console.log(`💚Assertion Passed: , ${actual.slice(1)}, === , ${expected}`);
+const tail = function (toTail){
+  let modTail = toTail.slice(1);
+  //console.log (modTail);
+}
+let actual = console.log(tail);
+const assertEqual = function(actual,expected) {
+  if (actual.length === expected.length) {
+    console.log(`💚Assertion Passed: , ${actual}, === , ${expected}`);
   }
   else {
-    console.log(`💔Assertion Failed: , ${actual.slice(1)}, !== , ${expected}`);
+    console.log(`💔Assertion Failed: , ${actual}, !== , ${expected}`);
   }
   return assertEqual;
 };
-
-
 assertEqual([2, 4 ,7], [4,7]);
-assertEqual(["mcd", "harvey", "kfc"], ["mcd", "harvery"]);
+assertEqual(["mcd", "harvey", "kfc"], ["mcd"]);
 assertEqual(["cat","dog","eguana"], ["dog","eguana"]);
-assertEqual([],0);
+assertEqual ([],[]);
